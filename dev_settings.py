a14a1534488cb0058c3a9cf7d15c71cd68d4e0db
Ben Dean-Kawamura <ben@pculture.org>
2018-01-23 09:22:44 -0500

    Don't load debug_toolbar for staging/production

--- dev_settings.py
+++ dev_settings.py
@@ -95,6 +95,38 @@ CACHE_TIMEOUT = 0
 
 COMPRESS_MEDIA = not DEBUG
 
+if DEBUG:
+    try:
+        import debug_toolbar
+    except ImportError:
+        pass
+    else:
+        INSTALLED_APPS += ('debug_toolbar',)
+        MIDDLEWARE_CLASSES = (
+            ('debug_toolbar.middleware.DebugToolbarMiddleware',) +
+            MIDDLEWARE_CLASSES
+        )
+        DEBUG_TOOLBAR_PATCH_SETTINGS = False
+
+        DEBUG_TOOLBAR_PANELS = (
+            'debug_toolbar.panels.timer.TimerPanel',
+            'debug_toolbar.panels.request.RequestPanel',
+            'debug_toolbar.panels.templates.TemplatesPanel',
+            'debug_toolbar.panels.sql.SQLPanel',
+            'caching.debug_toolbar_panels.CachePanel',
+        )
+
+        def custom_show_toolbar(request):
+            return 'debug_toolbar' in request.GET
+
+        DEBUG_TOOLBAR_CONFIG = {
+            'INTERCEPT_REDIRECTS': False,
+            'SHOW_TOOLBAR_CALLBACK': 'dev_settings.custom_show_toolbar',
+            'EXTRA_SIGNALS': [],
+            'HIDE_DJANGO_SQL': False,
+            'INSERT_BEFORE': '</body>',
+        }
+
 try:
     from dev_settings_local import *
 except ImportError:
