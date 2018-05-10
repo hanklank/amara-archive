4d29e17f330a1004c62c226011d775334d6b5e2d
Dan Antonio Reyes <danantonioreyes@gmail.com>
2018-05-10 15:42:25 +0800

    Added team check for lang/proj manager

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -721,6 +721,12 @@ class Team(models.Model):
         member = self.get_member(user)
         return bool(member and member.is_a_project_manager())
 
+    def user_is_a_language_or_project_manager(self, user):
+        member = self.get_member(user)
+        return bool(member and 
+                    (member.is_a_project_manager() or
+                     member.is_a_language_manager()))
+
     def user_is_any_type_of_manager(self, user):
         member = self.get_member(user)
         return member.is_any_type_of_manager()
