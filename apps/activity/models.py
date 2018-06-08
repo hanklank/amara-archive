9aaa89ba3fde010adf427d2bd10144b626ff79e4
Ben Dean-Kawamura <ben@pculture.org>
2018-06-08 10:28:26 -0400

    Removed duplicate queryset/manager code
    Moved all methods that could be useful to filter any query an in the
    QuerySet class.  Then used the as_manager()/from_queryset() methods to
    also add them to the manager classes.
    
    Renamed some classes to make them all use the capitalization "QuerySet"

--- apps/activity/models.py
+++ apps/activity/models.py
@@ -895,7 +895,7 @@ activity_choices = [
     VideoMovedToTeam, VideoMovedFromTeam, TeamSettingsChanged, SubtitleLanguageChanged,
 ]
 
-class ActivityQueryset(query.QuerySet):
+class ActivityQuerySet(query.QuerySet):
     def original(self):
         # For some reason, using copied_from__isnull=True results in an extra
         # join.  So we need a custom WHERE clause
@@ -923,16 +923,6 @@ class ActivityQueryset(query.QuerySet):
 
         return self.filter(q)
 
-class ActivityManager(models.Manager):
-    use_for_related_fields = True
-
-    def get_queryset(self):
-        return (ActivityQueryset(self.model, using=self._db)
-                .select_related('user', 'team', 'video'))
-
-    def original(self):
-        return self.get_queryset().original()
-
     def for_video(self, video, team=None):
         qs = self.filter(video=video).original()
         if team is None:
@@ -954,6 +944,13 @@ class ActivityManager(models.Manager):
 
     def for_team(self, team):
         return self.filter(team=team)
+
+class ActivityManager(models.Manager):
+    use_for_related_fields = True
+
+    def get_queryset(self):
+        return (ActivityQuerySet(self.model, using=self._db)
+                .select_related('user', 'team', 'video'))
 
     def create(self, type, **attrs):
         return super(ActivityManager, self).create(type=type, **attrs)
@@ -1139,7 +1136,7 @@ class ActivityRecord(models.Model):
     # copying behavior above and not include it in the default video listing.
     private_to_team = models.BooleanField(default=False, blank=True)
 
-    objects = ActivityManager()
+    objects = ActivityManager.from_queryset(ActivityQuerySet)()
 
     class Meta:
         ordering = ['-created', '-id']
